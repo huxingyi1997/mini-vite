e63583458ae95042a71029f3796e83716153db44
huxingyi1997 <huxingyi1997@sina.com>
2023-12-02 14:09:01 +0800

    feat: middleware support

--- src/node/server/index.ts
+++ src/node/server/index.ts
@@ -5,14 +5,44 @@ import connect from "connect";
 import { blue, green } from "picocolors";
 
 import { optimize } from "../optimizer";
+import { resolvePlugins } from "../plugins";
+import { createPluginContainer, PluginContainer } from "../pluginContainer";
+import { Plugin } from "../plugin";
+import { indexHtmlMiddware } from "./middlewares/indexHtml";
+
+export interface ServerContext {
+  root: string;
+  pluginContainer: PluginContainer;
+  app: connect.Server;
+  plugins: Plugin[];
+}
 
 export async function startDevServer() {
   const app = connect();
   const root = process.cwd();
   const startTime = Date.now();
+
+  const plugins = resolvePlugins();
+  const pluginContainer = createPluginContainer(plugins);
+
+  const serverContext: ServerContext = {
+    root: process.cwd(),
+    app,
+    pluginContainer,
+    plugins,
+  };
+
+  for (const plugin of plugins) {
+    if (plugin.configureServer) {
+      await plugin.configureServer(serverContext);
+    }
+  }
+  // 处理入口 HTML 资源
+  app.use(indexHtmlMiddware(serverContext));
+
   app.listen(3000, async () => {
     await optimize(root);
-    
+
     console.log(
       green("🚀 No-Bundle 服务已经成功启动!"),
       `耗时: ${Date.now() - startTime}ms`
@@ -20,3 +50,5 @@ export async function startDevServer() {
     console.log(`> 本地访问路径: ${blue("http://localhost:3000")}`);
   });
 }
+
+export interface ServerContext {}
